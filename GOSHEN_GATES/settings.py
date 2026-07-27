a8f35122da14baa98170951f5e48cc65decefd39
demiladelarunsi-bit <demiladelarunsi@gmail.com>
2026-07-27 16:12:59 +0100

    Update settings.py

--- GOSHEN_GATES/settings.py
+++ GOSHEN_GATES/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'GOSHEN_GATES.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,8 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'django.middleware.csrf.CsrfViewMiddleware',
-                # 'django.contrib.auth.middleware.AuthenticationMiddleware',
             ],
         },
     },
