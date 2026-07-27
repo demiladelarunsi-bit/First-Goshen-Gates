bf067fb775ce9183939d6797777cac6742fcc9f3
demiladelarunsi-bit <demiladelarunsi@gmail.com>
2026-07-27 15:23:43 +0100

    Update settings.py

--- GOSHEN_GATES/settings.py
+++ GOSHEN_GATES/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_uct^+#*$8=@e8it4!b_!apgo^()q-1svp5cnpgfjdimmuw5(e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
